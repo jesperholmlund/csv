472216bbdef8667ed6ece0baa0472eca83190a8c
jesperholmlund <jesper.holmlund@student.kyh.se>
2021-12-17 00:07:56 +0100

    masonry

--- views/includes/javascript.js
+++ views/includes/javascript.js
@@ -5,11 +5,16 @@ class Cards {
   }
 }
 
-const repeat = [];
+let repeat = [];
+let startBtn = document.getElementById("start");
+startBtn.disabled = true;
 
 let cards = document.querySelectorAll(".card");
 cards.forEach((card, i, arr) => {
   card.addEventListener("click", () => {
+    if ((startBtn.disabled = true)) {
+      startBtn.disabled = false;
+    }
     card.classList.toggle("bg-warning");
     let cardTxt = card.querySelector(".card-body").innerText;
     if (repeat.find((repeatCard) => repeatCard.id == i) == undefined) {
@@ -24,19 +29,42 @@ cards.forEach((card, i, arr) => {
 
 const cardListRepeat = document.getElementById("cardListRepeat");
 const cardList = document.getElementById("cardList");
-let startBtn = document.getElementById("start");
 startBtn.addEventListener("click", () => {
+  cardList.querySelectorAll(".card").forEach((card) => {
+    card.classList.remove("bg-warning");
+  });
+  let i = 0;
   cardListRepeat.innerHTML = "";
-  repeat.forEach((repeat) => {
-    cardListRepeat.innerHTML += `<div class="card" style="width: 30em; height: 20em;" >
+  cardListRepeat.innerHTML = `<div id="card-${repeat[i].id}" class="card" style="width: 30em;" >
     <div class="card-header"></div>
     <div class="card-body align-middle">
-      <p class="card-text">${repeat.card}</p>
+      <p class="card-text">${repeat[i].card}</p>
     </div>
     <div class="card-footer">
+    <button class="btn nextBtn">Next</button>
     </div>
   </div>`;
+  let nextBtns = document.querySelectorAll(".nextBtn");
+  nextBtns.forEach((nextBtn) => {
+    nextBtn.addEventListener("click", () => {
+      i++;
+      if (repeat[i] == undefined) {
+        startBtn.disabled = false;
+        cardList.style.display = "flex";
+        repeat = [];
+        document.getElementById("cardCounter").innerHTML = repeat.length;
+        startBtn.disabled = true;
+        cardListRepeat.innerHTML = "";
+      } else if (repeat[i] != undefined) {
+        nextBtn.parentNode.parentNode.querySelector(".card-body").innerText =
+          repeat[i].card;
+      }
+    });
   });
   startBtn.disabled = true;
   cardList.style.display = "none";
+});
+FlexMasonry.init(".grid", {
+  responsive: false,
+  numCols: 3,
 });
